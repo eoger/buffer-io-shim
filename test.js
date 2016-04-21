f49b71346dc7f4e471ca102b4c8d02773a46a33f
Edouard Oger <eoger@fastmail.com>
2016-04-21 15:10:03 -0700

    Initial commit

--- test.js
+++ test.js
@@ -1,34 +1,18 @@
 require('./');
-var A = Buffer('123');
-var B = Buffer('0123');
-var C = Buffer('xyz');
+var assert = require('assert');
+var buf;
 
-console.log('Buffer.prototype.compare : ');
-
-console.log('should return 0 when a byte sequences matches and buffers are of equal length');
-console.assert(A.compare(A) === 0, 'Failed:' + A.compare(A));
-console.log('Passed');
-console.log('should return 1 when a byte sequence matches and the left buffer is smaller');
-console.assert(A.compare(B) === 1, 'Failed:' + A.compare(B));
-console.log('Passed');
-console.log('should return 1 when a byte sequence matches and the left buffer is larger');
-console.assert(B.compare(A) === -1, 'Failed:' + B.compare(A));
-console.log('Passed');
-console.log('should return -1 when there is no match');
-console.assert(A.compare(C) === -1, 'Failed:' + A.compare(C));
+console.log('Buffer.prototype.readUIntLE : ');
+buf = new Buffer([0x01, 0x02, 0x03, 0x04, 0x05, 0x06]);
+assert.equal(buf.readUIntBE(0, 1), 0x01);
+assert.equal(buf.readUIntBE(0, 3), 0x010203);
+assert.equal(buf.readUIntBE(0, 5), 0x0102030405);
+assert.equal(buf.readUIntBE(0, 6), 0x010203040506);
 console.log('Passed');
 
-console.log('Buffer.compare : ');
-
-console.log('should return 0 when a byte sequences matches and buffers are of equal length');
-console.assert(Buffer.compare(A, A) === 0, 'Failed:' + Buffer.compare(A, A));
-console.log('Passed');
-console.log('should return 1 when a byte sequence matches and the left buffer is smaller');
-console.assert(Buffer.compare(A, B) === 1, 'Failed:' + Buffer.compare(A, B));
-console.log('Passed');
-console.log('should return 1 when a byte sequence matches and the left buffer is larger');
-console.assert(Buffer.compare(B, A) === -1, 'Failed:' + Buffer.compare(B, A));
-console.log('Passed');
-console.log('should return -1 when there is no match');
-console.assert(Buffer.compare(A, C) === -1, 'Failed:' + Buffer.compare(A, C));
+console.log('Buffer.prototype.writeUIntBE : ');
+buf = Buffer(3);
+buf.writeUIntBE(0x123456, 0, 3);
+assert.deepEqual(buf.toJSON(), [0x12, 0x34, 0x56]);
+assert.equal(buf.readUIntBE(0, 3), 0x123456);
 console.log('Passed');
